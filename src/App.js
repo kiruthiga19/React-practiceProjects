7adc2e5a4a75d0418d8da572e0da26333606df75
kiruthiga19 <99905519+kiruthiga19@users.noreply.github.com>
2022-06-03 00:06:24 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1>Hello welcom to react UI </h1>
-        <button  id="link"><a href="https://reactjs.org"></a> click to begin</button>
-      </header>
-    </div>
-  );
+import React from "react";
+import "./index.css";
+export default function App(){
+    return(
+        <div>
+            <h1>hello welcome to react</h1>
+            <p>let  learn together</p>
+        </div>
+    )
 }
-
-export default App;
